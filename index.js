f500af2ad1bb8dc00e14fe1ad10d3e31397f0a17
saifhossainjibon <saifhossainjibon@gmail.com>
2022-09-17 08:42:45 -0700

    port renamee

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x4r9eu9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// console.log(uri)
+
 async function run() {
     try {
         await client.connect();
