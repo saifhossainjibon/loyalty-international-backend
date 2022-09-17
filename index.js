d7080b1a8f1aeeaf1feb061705084e3af98f8de0
saifhossainjibon <saifhossainjibon@gmail.com>
2022-09-17 08:32:25 -0700

    port change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
